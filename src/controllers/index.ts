a61625d0f290e8d03bac7df0d21914d5e84ceb2f
CREDO23 <thierrybakera12@gmail.com>
2024-03-01 23:26:39 +0200

    feat: add redirections from short url

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,21 +1,22 @@
-import { NextFunction , Request, Response} from "express";
-import Link from "../models/link";
+import { NextFunction, Request, Response } from 'express';
+import Link from '../models/link';
 
 export const redirect = async (
-    req: Request,
-    res: Response,
-    next: NextFunction,
-  ): Promise<void> => {
-    try {
-      const { maker } = req.params;
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const { maker } = req.params;
 
-      const {long_form} = await Link.findOne({where : {maker}})
+    const link = await Link.findOne({ where: { maker } });
 
-      res.redirect(long_form)
-    } catch (error) {
-      next(error);
-    }
-  };
+    link.visit_times = link.visit_times + 1;
 
+    link.save();
 
-
+    res.redirect(link.long_form);
+  } catch (error) {
+    next(error);
+  }
+};
